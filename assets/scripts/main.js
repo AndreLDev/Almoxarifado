386b1437629152d87255b8a9989ea21ecf871db4
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2023-12-15 17:00:26 -0300

    btnRemover

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,27 +17,27 @@ function adicionarCorAoFocarInput() {
     const ListInput = document.querySelectorAll('input')
     console.log(ListInput.length);
     console.log(ListInput);
-    
+
     // Array.prototype.map.call(ListInput, (object)=>{
     //     object.style.backgroundColor='#98FB98'
     // });
 
 
-    ListInput.forEach((object)=>{
-        object.addEventListener('focus', ()=>{
-            object.style.backgroundColor='#98FB98'
+    ListInput.forEach((object) => {
+        object.addEventListener('focus', () => {
+            object.style.backgroundColor = '#98FB98'
         });
 
-        object.addEventListener('blur', ()=>{
-            object.style.backgroundColor='white'
+        object.addEventListener('blur', () => {
+            object.style.backgroundColor = 'white'
         });
     })
 }
 
 
-function LoadCate(){
+function LoadCate() {
     const selectCate = document.getElementById('categoriaMotivo');
-    selectCate.innerHTML="";
+    selectCate.innerHTML = "";
 
     const optFirst = document.createElement('option');
     optFirst.value = 0;
@@ -45,7 +45,7 @@ function LoadCate(){
 
     selectCate.add(optFirst);
 
-    categorias.forEach((Categoria)=>{
+    categorias.forEach((Categoria) => {
         const opt = document.createElement('option');
         opt.value = Categoria.idCategoria;
         opt.text = Categoria.Descricao;
@@ -55,9 +55,9 @@ function LoadCate(){
 }
 
 
-function LoadMotivos(){
+function LoadMotivos() {
     const selectCate = document.getElementById('Motivo');
-    selectCate.innerHTML="";
+    selectCate.innerHTML = "";
 
     const optFirst = document.createElement('option');
     optFirst.value = 0;
@@ -67,9 +67,9 @@ function LoadMotivos(){
 
     const categoriaValue = document.getElementById('categoriaMotivo').value;
     console.log(categoriaValue);
-    const motivoFiltrado = motivos.filter((e)=> e.idCategoria == categoriaValue)
+    const motivoFiltrado = motivos.filter((e) => e.idCategoria == categoriaValue)
 
-    motivoFiltrado.forEach((motivo)=>{
+    motivoFiltrado.forEach((motivo) => {
         const opt = document.createElement('option');
         opt.value = motivo.idCategoria;
         opt.text = motivo.Descricao;
@@ -78,7 +78,7 @@ function LoadMotivos(){
     });
 }
 
-document.getElementById('categoriaMotivo').addEventListener('change', ()=>{
+document.getElementById('categoriaMotivo').addEventListener('change', () => {
     LoadMotivos();
 });
 
@@ -88,11 +88,11 @@ adicionarCorAoFocarInput();
 
 
 
+var count = 0;
 
-
-function exibirDadosTabela(id, produto, estoque, quantidade){
+function exibirDadosTabela(id, produto, estoque, quantidade) {
     var totalDeTudo = 0;
-    produtos.forEach((Produto)=>{
+    produtos.forEach((Produto) => {
         const codigo = Produto.idProduto;
         const produto = Produto.Descricao;
         const Qtd = Produto.Estoque;
@@ -100,32 +100,47 @@ function exibirDadosTabela(id, produto, estoque, quantidade){
         const Preco = Produto.Valor;
         const Total = Produto.Valor * Produto.Estoque;
 
-        if(codigo == id){
+        if (codigo == id) {
             document.getElementById("DescricaoProduto").value = produto;
             document.getElementById("Estoque").value = Qtd
         }
-        
-        if(codigo == id && quantidade <= Qtd && quantidade != ''){
-            const stringAux = "<tr><td>"+codigo+"</td><td>"+produto+"</td><td>"+Qtd+"</td><td>"+quantidade+"</td><td>R$"+Preco+"</td><td>R$"+Preco*quantidade+"</td><td><button id='test'>Remover</button></td></tr>";
+
+        if (codigo == id && quantidade <= Qtd && quantidade != '') {
+            const stringAux = "<tr id='teste"+ count +"'><td>" + codigo + "</td><td>" + produto + "</td><td>" + Qtd + "</td><td>" + quantidade + "</td><td>R$" + Preco + "</td><td>R$" + Preco * quantidade + "</td><td><button class='BtnRemoverItens' onclick='remover("+ count +", "+ Preco * quantidade +" , "+ codigo +" , "+ quantidade +")'>Remover</button></td></tr>";
             document.getElementById("tabelaTeste").innerHTML += stringAux;
+            count += 1;
 
             Produto.Estoque = Qtd - quantidade;
-            if(document.getElementById("total").value != ''){
-                document.getElementById("total").value = parseInt(document.getElementById("total").value) + Preco*quantidade;
-            }else{
-                document.getElementById("total").value = Preco*quantidade;
-            }
+            document.getElementById("Estoque").value = Produto.Estoque;
+
             
+            if (document.getElementById("total").value != '') {
+                document.getElementById("total").value = parseInt(document.getElementById("total").value) + Preco * quantidade;
+            } else {
+                document.getElementById("total").value = Preco * quantidade;
+            }
+
         }
-        
+
     });
-        
-    
+
+
 }
 
+function remover(id, total, codigo, quantidade){
+    document.getElementById("teste"+ id).remove();
+    document.getElementById("total").value = parseInt(document.getElementById("total").value) - total;
 
+    const produto = produtos.filter((e) => e.idProduto == codigo);
 
-function adicionarProduto(){
+    produto[0].Estoque += quantidade;
+    document.getElementById("Estoque").value = produto[0].Estoque;
+    document.getElementById("DescricaoProduto").value = produto[0].Descricao;
+    document.getElementById("CodigoProduto").value = produto[0].idProduto;
+
+}
+
+function adicionarProduto() {
     var id = document.getElementById("CodigoProduto").value
     var produto = document.getElementById("DescricaoProduto").value
     var estoque = document.getElementById("Estoque").value
